583de08cfa524215887b2c0f23c1e36f3c1fdb3c
SanthoshSelvam <97081147+santhosh15071998@users.noreply.github.com>
2022-12-01 15:28:51 +0530

    Add files via upload

---
+++ hero.php
@@ -0,0 +1,16 @@
+<?php 
+
+{
+  "id": "hero",
+  "title": "INNOVATE AND ADAPT FASTER",
+  "content": "Tachyon is an award-winning cognitive assistant platform for Software Engineers to develop enterprise-grade quality software solutions swiftly with minimum efforts. Tachyon understands any human native language and converts software diagrams into relevant programs. Tachyon augments end-users by providing context-specific suggestions, recommendations, discovery, and intelligent automation.",
+  "imageUrl": "",
+  "background": "rgb(255,192,0)",
+  "width": "100vw",
+  "height": "100vh",
+  "action": {
+    "text": "No Credit cards. Sign up here for get demo link",
+    "label": "Get Demo",
+    "link": "https://www.tachyonsys.com.au/book-demo"
+  }
+},
